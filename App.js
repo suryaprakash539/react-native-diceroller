35bf80d5289965adfc60d990149307c63a2d99d5
suryaprakash <surya.zvky@gmail.com>
2020-11-26 15:58:13 +0530

    styles and button added to play with dice

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, TouchableOpacity, View, Image} from 'react-native';
+import {Text, TouchableOpacity, View, Image, StyleSheet} from 'react-native';
 
 import DiceOne from './assets/dice1.png';
 import DiceTwo from './assets/dice2.png';
@@ -12,10 +12,10 @@ const App = () => {
   const uri = DiceFive;
   return (
     <>
-      <View>
-        <Image />
+      <View style={styles.container}>
+        <Image style={styles.image} source={uri} />
         <TouchableOpacity>
-          <Text>DiceRoller</Text>
+          <Text style={styles.gamePlayButton}>DiceRoller</Text>
         </TouchableOpacity>
       </View>
     </>
@@ -23,3 +23,25 @@ const App = () => {
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgb(32,0,216)',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  gamePlayButton: {
+    marginTop: 20,
+    padding: 10,
+    fontSize: 20,
+    color: 'white',
+    borderRadius: 5,
+    borderWidth: 2,
+    backgroundColor: 'lightcoral',
+  },
+});
